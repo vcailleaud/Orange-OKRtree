e2a02d4f82f8b30caf70733edf2da5248a5f698c
Vincent Cailleaud <vincent.cailleaud@triskellsoftware.fr>
2023-10-18 18:13:55 +0200

    fixing call

--- server.js
+++ server.js
@@ -107,7 +107,47 @@ var server = http.createServer(function(req, res) {
 												}
 												
 												//Google chart test
-												var html = `<!doctype html>
+												var html1 = `<html>
+															<head>
+															<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+															<script type="text/javascript">
+																google.charts.load('current', {'packages':['corechart']});
+																google.charts.setOnLoadCallback(drawChart);
+														
+																function drawChart() {
+														
+																var data = google.visualization.arrayToDataTable([
+																	['Task', 'Hours per Day'],
+																	['Work',     11],
+																	['Eat',      2],
+																	['Commute',  2],
+																	['Watch TV', 2],
+																	['Sleep',    7]
+																]);
+														
+																var options = {
+																	title: 'My Daily Activities'
+																};
+														
+																var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+														
+																chart.draw(data, options);
+																}
+															</script>
+															</head>
+															<body>
+															<div id="piechart" style="width: 900px; height: 500px;"></div>
+															</body>
+														</html>
+														`;
+
+
+												
+
+												
+
+												//tree boxes test
+												var html2 = `<!doctype html>
 															<html>
 															<head>
 																<meta charset="utf-8">
@@ -138,7 +178,7 @@ var server = http.createServer(function(req, res) {
 												
 												
 												res.writeHead(200,{'Content-Type': 'text/html'});
-												res.write(html);
+												res.write(html2);
 												res.end();
 												console.log('OK');
 												}
